60471815e18998a66ec0aba9a6bf5ef5de0f878e
Jair <jair.terrones429@gmail.com>
2019-11-21 20:44:01 -0500

    realizando los componentes de estado

--- src/App.js
+++ src/App.js
@@ -1,64 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 // import './App.css';
 import "./styles/styles.scss";
-import Curso from "./card";
-
-const cursos = [
-  {
-    title: "Curso de go",
-    image:
-      "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/go_0.jpg?itok=k2amLhrN",
-    price: 50,
-    profesor: "Alexys lozada"
-  },
-  {
-    title: "Java",
-    image:
-      "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/java%20desde%20cero.jpg",
-    price: 80,
-    profesor: "Alexys lozada"
-  },
-  {
-    title: "HTML",
-    image:
-      "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/HTML-2018.jpg?itok=Gyvm-W9t",
-    price: 10,
-    profesor: "Alvaro Felipe"
-  }
-];
+// import Curso from "./card";
+import Banner from "./banner"
+import Formulario from "./formulario"
 
 const App = () => (
-  <Fragment>
-    <div className="main-banner img-container l-section" id="main-banner">
-      <div className="ed-grid lg-grid-6">
-        <div className="lg-cols-4 lg-x-2">
-          <img
-            className="main-banner__img"
-            src="https://images.pexels.com/photos/771881/pexels-photo-771881.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            alt="imagen de newyork"
-          />
-          <div className="main-banner__data s-center">
-            <p className="t2 s-mb-0">Título del banner</p>
-            <p> Subtítulo del banner</p>
-            <a href="/" className="button">
-              Botón del banner
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="ed-grid m-grid-3">
-      {cursos.map((c, index) => (
-        <Curso key={index}
-          title={c.title}
-          image={c.image}
-          price={c.price}
-          profesor={c.profesor}
-        />
-      ))}
-    </div>
-  </Fragment>
+  <>
+    <Banner />
+    <Formulario name='EDteam'/>
+  </>
 );
 
 export default App;
